4b2a2f54d08da889620c4b2afba99746d550dfd9
RikakoMiura <rmiura2016@gmail.com>
2021-09-15 14:06:28 +0000

    [Add]Meshiterro

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '9e8a9fc643f0c68ab475f0588c3f29b8742a6a7231d011bfd39859d1b26576865be035632d63b14860e9d74d144fb7903add8e808ff2d1a932e715e87255a79a'
